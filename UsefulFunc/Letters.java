c3cff0f2dfb824952124559174bc0de171334c6d
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-20 21:12:08 +0300

    Create Letters.java

---
+++ UsefulFunc/Letters.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.ptint("Введите число: ");
+    ont n - in.nextInt();
+    int i,j;
+    for (i=1; i<=n; i++)
+    {
+      for (j=1; j<=i; j++)
+      { 
+        System.out.print((char)(j+64));
+        System.out.print((char)(j+96));
+      }
+      System.out.print("");
+    }
+  }
+}
