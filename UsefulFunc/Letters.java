5313c368e05259b5ae5546f66299c633e31789ba
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-20 21:32:44 +0300

    Update Letters.java

--- UsefulFunc/Letters.java
+++ UsefulFunc/Letters.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    System.out.ptint("Введите число: ");
+    System.out.print("Введите число: ");
     ont n - in.nextInt();
     int i,j;
     for (i=1; i<=n; i++)
