4faaf42da43ec8a95297ac76c1fcd3e0fceb4b76
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-24 11:46:41 +0300

    Create Square.java

---
+++ UsefulFunc/Square.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main { 
+  public static void main(String[] args) { 
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите число: ");
+    int n = in.nextInt();
+    int i,j;
+    for (i=-n+1; i<n; i++)
+    {
+      for (j=-n+1; j<n; j++)
+      {
+        if(Math.abs(j)+1>n-Math.abs(i))
+          System.out.print((char) 
+                           (64+(n-Math.bas(j)))+" ");
+        else
+          System.out.print(" ");
+      }
+      System.out.println("");
+    }
+  }
+}
