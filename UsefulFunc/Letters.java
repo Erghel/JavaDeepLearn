17275dde7ac4b161d4218c9cb5fbf0bc64b5e700
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-24 12:40:56 +0300

    Update Letters.java

--- UsefulFunc/Letters.java
+++ UsefulFunc/Letters.java
@@ -6,9 +6,10 @@ public class Main {
     System.out.print("Введите число: ");
     int n = in.nextInt();
     int i,j;
-    for (i=1; i<=n; i++)
+    
+    for (i = 1; i <= n; i++)
     {
-      for (j=1; j<=i; j++)
+      for (j = 1; j <= i; j++)
       { 
         System.out.print((char)(j+64));
         System.out.print((char)(j+96));
