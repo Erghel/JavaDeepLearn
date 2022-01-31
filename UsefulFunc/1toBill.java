782a202417b02fddcee536aa13270bde290dd49b
Erghel <88831850+Erghel@users.noreply.github.com>
2022-02-01 00:28:33 +0300

    Create 1toBill.java

---
+++ UsefulFunc/1toBill.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class sum {
+	public static void main(String[] args) {
+		int n = 1000000000;
+		BigInteger ans = BigInteger.ZERO;
+    
+		for(int i = 1; i <= n; i++)
+			ans = ans.add(BigInteger.valueOf(i));
+		System.out.println(ans);
+	}
+}
